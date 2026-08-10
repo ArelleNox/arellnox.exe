732780dec7548a5fc6c5dd3a279f72c73832a2b3
Janelle <janelleroxane@gmail.com>
2026-08-11 00:18:15 +0200

    database-backed admin panel. enabeling the writing post ability, created an admin profile

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post 
+@admin.register(Post) 
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin): 
+    list_display = ("title", "language", "category", "is_published", "created_at") 
+    list_filter = ("language", "category", "is_published") 
+    prepopulated_fields = {"slug": ("title",)}
